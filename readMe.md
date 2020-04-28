49a55afc1d87f02fffc95b6ed4dae48b1274a1b8
hsjeevan <hsjeevan07@gmail.com>
2020-04-28 22:03:48 +0530

    splash-screen-working

---
+++ readMe.md
@@ -0,0 +1,12 @@
+ ### Copy paste this in config.xml file
+ 
+ ```<preference name="SplashMaintainAspectRatio" value="true" />
+    <preference name="FadeSplashScreenDuration" value="300" />
+    <preference name="SplashShowOnlyFirstTime" value="false" />
+    <preference name="SplashScreen" value="screen" />
+    <preference name="SplashScreenDelay" value="10000" />
+    <preference name="AutoHideSplashScreen" value="false" />
+    <preference name="FadeSplashScreen" value="false" />
+    <preference name="ShowSplashScreen" value="false" />
+    <preference name="ShowSplashScreenSpinner" value="false" />```
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,27 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  showSplash: boolean = true;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
       this.splashScreen.hide();
+      timer(4000).subscribe(() => this.showSplash = false) // <-- hide animation after 3s
     });
   }
 }
